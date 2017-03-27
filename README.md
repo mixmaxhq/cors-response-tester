5eb8715e8d6e1676c244caadd72fcd88243074ff
Jeff Wear <jeff@mixmax.com>
2017-03-27 02:06:36 -0500

    Update for https://github.com/expressjs/cors/pull/110

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # cors-response-tester
 
 Testing whether the cors module runs request handlers when the origin doesn't match,
-for https://github.com/expressjs/cors/issues/109.
+for https://github.com/expressjs/cors/issues/109 and https://github.com/expressjs/cors/pull/110.
 
 # Installation
 
@@ -16,7 +16,7 @@ a successful response) and http://underscorejs.org/ (to get an error):
 
 ```js
 var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://localhost:9999/something-expensive', true);
+xhr.open('POST', 'http://localhost:9999/something-expensive', true);
 xhr.addEventListener('error', () => console.error('error'));
 xhr.addEventListener('load', () => console.log('response:', xhr.responseText));
 xhr.send();
@@ -25,3 +25,6 @@ xhr.send();
 But note that the terminal logged "running" *twice*, i.e. the cors module still called `next` for
 the request from http://underscorejs.org/, even though it did not set the response headers to allow
 the browser to read the response.
+
+Now set `mismatchContinue: true` and try running the snippet from http://underscorejs.org again.
+Note that "running" is _not_ logged again.
--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 /* jshint esnext: true */
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
-// jQuery chosen just because it has a permissive CSP, so that the XHR snippet from the README
-// can be run in the console.
-const jqueryCors = cors({ origin: 'http://jquery.com' });
+const cors = require('cors')({
+  // jQuery chosen just because it has a permissive CSP, so that the XHR snippet from the README
+  // can be run in the console.
+  origin: ['http://localhost:9999', 'http://jquery.com'],
+  mismatchContinue: true // The default.
+});
 
-app.get('/something-expensive', jqueryCors, (req, res) => {
+app.post('/something-expensive', cors, (req, res) => {
   console.log('running');
   res.send('foo');
+});
+
+app.get('/view', (req, res) => {
+  res.send(`
+    <button style="font-size: 14px;">Click to test same-origin request</button>
+    <script>
+      document.querySelector('button').addEventListener('click', () => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', '/something-expensive', true);
+        xhr.addEventListener('error', () => console.error('error'));
+        xhr.addEventListener('load', () => console.log('response:', xhr.responseText));
+        xhr.send();
+      });
+    </script>
+  `);
 });
 
 app.listen(9999);
